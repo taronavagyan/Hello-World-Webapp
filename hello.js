05c6b61a6d03b1c58d0e2f148612a7d56054be75
Taron Avagyan <contact@taronavagyan.com>
2022-12-20 13:15:18 -0800

    refactoring

--- hello.js
+++ hello.js
@@ -2,30 +2,79 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+const COUNTRY_DATA = [
+  {
+    path: "/english",
+    flag: "flag-of-United-States-of-America.png",
+    alt: "US Flag",
+    title: "Go to US English site",
+  },
+
+  {
+    path: "/french",
+    flag: "flag-of-France.png",
+    alt: "Drapeau de la france",
+    title: "Aller sur le site français",
+  },
+
+  {
+    path: "/serbian",
+    flag: "flag-of-Serbia.png",
+    alt: "Застава Србије",
+    title: "Идите на српски сајт",
+  },
+
+  {
+    path: "/greek",
+    flag: "flag-of-Greece.png",
+    alt: "Η Γη",
+    title: "Πηγαίνετε στον ελληνικό ιστότοπο",
+  },
+];
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
 app.use(morgan("common"));
 
+app.locals.currentPathClass = (path, currentPath) => {
+  return path === currentPath ? "current" : "";
+};
 app.get("/", (req, res) => {
   res.redirect("/english");
 });
 
 app.get("/english", (req, res) => {
-  res.render("hello-world-english");
+  res.render("hello-world-english", {
+    countries: COUNTRY_DATA,
+    currentPath: req.path,
+    language: "en-US",
+  });
 });
 
 app.get("/french", (req, res) => {
-  res.render("hello-world-french");
+  res.render("hello-world-french", {
+    countries: COUNTRY_DATA,
+    currentPath: req.path,
+    language: "fr-FR",
+  });
 });
 
 app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian");
+  res.render("hello-world-serbian", {
+    countries: COUNTRY_DATA,
+    currentPath: req.path,
+    language: "sr-Cyrl-rs",
+  });
 });
 
 app.get("/greek", (req, res) => {
-  res.render("hello-world-greek");
+  res.render("hello-world-greek", {
+    countries: COUNTRY_DATA,
+    currentPath: req.path,
+    language: "el-GR",
+  });
 });
 
 app.listen(3000, "localhost", () => {
